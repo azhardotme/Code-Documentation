449b95c7a72c48ef0e7c64fa1d3430aa71fa004a
Azharul Islam <32319631+azhardotme@users.noreply.github.com>
2020-12-03 02:59:32 +0600

    Code Documentation

---
+++ index.js
@@ -0,0 +1,13 @@
+
+/**
+ * addNumbers function will add two numbers
+ * @param {number} x - first number
+ * @param {number} y - second number 
+ */
+
+function addNumbers(x,y){
+    console.log(x+y);
+    }
+    
+    addNumbers(2,5);
+    
